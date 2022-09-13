35ac0f56e23f5e16be351703412ca760ef07fa65
Ali Rilwan <a.rilwan@aasandha.mv>
2022-09-13 16:03:12 +0500

    setting

--- resources/views/eng/setting.blade.php
+++ resources/views/eng/setting.blade.php
@@ -2,6 +2,8 @@
 @extends('/eng/baseTemp')
 @section('content')   
 
+
+
     <!-- users details -->
     @if(auth()->user()->rank == "OWNER" )
     <div class="ContContainer" id="users">
@@ -14,11 +16,11 @@
                 <div class="col col-4">Status</div>
             </li>
             @foreach($users as $user)
-            <li class="table-row" onclick="{{$user->id}}">
-                <div class="col col-1" data-label="Des">{{$user->fullname}}</div>
-                <div class="col col-2" data-label="U-Price">{{$user->rank}}</div>
-                <div class="col col-2" data-label="U-Price">{{$user->contact}}</div>
-                <div class="col col-4" data-label="S-Total" onclick="">{{$user->status === 1?'has Access':'No Access'}}</div>
+            <li class="table-row" onclick="NewUser('{{$user->id}}')">
+                <div class="col col-1" id="name{{$user->id}}" data-label="Des">{{$user->fullname}}</div>
+                <div class="col col-2" id="rank{{$user->id}}" data-label="U-Price">{{$user->rank}}</div>
+                <div class="col col-2" id="contact{{$user->id}}" data-label="U-Price">{{$user->contact}}</div>
+                <div class="col col-4" id="status{{$user->id}}" data-label="S-Total" >{{$user->status === 1?'has Access':'No Access'}}</div>
             </li>
             @endforeach
             <div class="MiniBtn middle" onClick="NewUser()"> + </div>
@@ -29,7 +31,6 @@
     <form action="/setting/change" method="POST" class="ContContainer" id="changePass">
         @csrf
         <h4>Change Password</h4> 
-        <a href="/setting"><button class="MiniBtn" onClick=""> clear </button></a>
         <input type="password" name="old_pass" placeholder="Old Password" class="inputField inputSmall" >
         <input type="password" name="new_pass" placeholder="New Password" class="inputField inputSmall" >
         <input type="password" name="con_pass" placeholder="Confirm Password"class="inputField inputSmall" >
@@ -38,16 +39,16 @@
 
     @if(auth()->user()->rank == "OWNER" )
     {{-- pop up form for creating new crew user --}}
-    <div class="loginForm popForm" id="popForm" style="display:none" >
-        @if ($errors->any())
-            <div id="alertMSG" class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
+    @if ($errors->any())
+        <div id="alertMSG" class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <div class="loginForm popForm" id="popForm" style="display:none; margin-top:20px; " >
         <form class="loginFormCont" action="/setting/user" method="POST">
             <h2>Create New User</h2>
             @csrf
@@ -56,23 +57,60 @@
             <input type="text" name="email" placeholder="Email" class="inputField">
             <input type="password" name="password" placeholder="Password" class="inputField">
             <input type="password" name="password_confirmation" placeholder="Password Confirmation" class="inputField">
-            <button type="submit" name="submit" class="FormBtn">Create</button>
+            <button type="submit" name="submit" class="FormBtn" onclick="saving()" >Create</button>
             <button onclick="NewUserClose()" class="FormBtn CancelBtn">Cancel</button>
+        </form>
+    </div>
+        
+    <div class="loginForm popForm" id="popFormEdit" style="display:none; margin-top:20px; " >
+        <form class="loginFormCont" id="editForm" action="/setting/user/" method="POST">
+            <h2>Edit User</h2>
+            @csrf
+            <input type="hidden" name="_method" value="patch">
+            <input type="text" id="name" name="Fname" placeholder="Full Name" class="inputField ">
+            <input type="number" id="contact" name="contact" placeholder="Mobile Number" class="inputField">
+            <input type="text" id="email" name="email" placeholder="Email" class="inputField">
+            <input type="password" name="password" placeholder="Password" class="inputField">
+            <input type="password" name="password_confirmation" placeholder="Password Confirmation" class="inputField">
+            <select name="Status" id="status"  class="inputField">
+                <option value="0">Remove Access</option>
+                <option value="1">has Access</option>
+            </select>
+            <button type="submit" name="submit" class="FormBtn">Update</button>
+            <button onclick="NewUserClose('out')" class="FormBtn CancelBtn">Cancel</button>
         </form>
         
     </div>
     @endif
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script>
-        function NewUser(){
-            document.getElementById('popForm').style.display='block';
+        function NewUser(input){
             document.getElementById('users').style.display='none';
             document.getElementById('changePass').style.display='none';
+            if(input){
+                document.getElementById('popFormEdit').style.display='block';
+                FillForm(input);
+            }else{
+                document.getElementById('popForm').style.display='block';
+                
+            }
         }
-        function NewUserClose(){
+        function NewUserClose(input){
             event.preventDefault();
-            document.getElementById('popForm').style.display='none';
+            if(input){
+                document.getElementById('popFormEdit').style.display='none';
+            }else{
+                document.getElementById('popForm').style.display='none';
+            }
             document.getElementById('users').style.display='block';
             document.getElementById('changePass').style.display='block';
+        }
+        function FillForm(id){
+            $('#name').val($('#name'+id).text());
+            $('#contact').val($('#contact'+id).text());
+            $('#name').val($('#name'+id).text());
+            $('#editForm').attr('action', '/setting/user/'+id);
+            $('#status'+id)!=='has Access' ? $('#status').val(1): $('#status').val(0);
         }
     </script>
 
